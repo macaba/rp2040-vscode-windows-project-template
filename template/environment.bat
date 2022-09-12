18be852a71d869ab17a69938a2c09a5f48566a58
macaba <1031306+macaba@users.noreply.github.com>
2022-09-12 14:08:43 +0100

    Added some optional config items

--- template/environment.bat
+++ template/environment.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set PICO_SDK_PATH=C:\Pico\pico-sdk
-set PICO_TOOLCHAIN_PATH=C:\Program Files (x86)\Arm GNU Toolchain arm-none-eabi\11.2 2022.02
+set PICO_TOOLCHAIN_PATH=C:\Program Files (x86)\GNU Arm Embedded Toolchain\10 2021.10
