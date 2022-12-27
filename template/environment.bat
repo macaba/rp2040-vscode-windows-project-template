7187be64ee1f645e7d25271225fb3bf0144615c6
macaba <1031306+macaba@users.noreply.github.com>
2022-12-27 16:35:25 +0000

    Fixes for latest toolchain version

--- template/environment.bat
+++ template/environment.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set PICO_SDK_PATH=C:\Pico\pico-sdk
-set PICO_TOOLCHAIN_PATH=C:\Program Files (x86)\GNU Arm Embedded Toolchain\10 2021.10
+set PICO_TOOLCHAIN_PATH=C:\Program Files (x86)\Arm GNU Toolchain arm-none-eabi\12.2 rel1\bin
