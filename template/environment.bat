f41d6ee6b2ee13957ff3f69fb0a46a622438a050
macaba <1031306+macaba@users.noreply.github.com>
2024-04-19 14:10:17 +0100

    Removed bat files

--- template/environment.bat
+++
@@ -1,3 +0,0 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set PICO_SDK_PATH=C:\Pico\pico-sdk
-set PICO_TOOLCHAIN_PATH=C:\Program Files (x86)\Arm GNU Toolchain arm-none-eabi\12.2 rel1\bin
